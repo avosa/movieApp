662b2c32ce07ef91bd517a00a902205a2a0034f9
avosa <websterb17@gmail.com>
2021-03-24 02:54:16 +0300

    Updates

---
+++ precache-manifest.ede0d282f63e1bfd095c4e0a6ca2081f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86239a3ecc9acf32cae826d589463a82",
+    "url": "/avosa/movieApp/index.html"
+  },
+  {
+    "revision": "cd05699923cbb1ac0554",
+    "url": "/avosa/movieApp/static/css/2.1fa805dd.chunk.css"
+  },
+  {
+    "revision": "c44e43270e54c05e331f",
+    "url": "/avosa/movieApp/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "cd05699923cbb1ac0554",
+    "url": "/avosa/movieApp/static/js/2.38710af1.chunk.js"
+  },
+  {
+    "revision": "c44e43270e54c05e331f",
+    "url": "/avosa/movieApp/static/js/main.b1b80742.chunk.js"
+  },
+  {
+    "revision": "b01dacdc99f48c0f15a3",
+    "url": "/avosa/movieApp/static/js/runtime~main.2a00618e.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/avosa/movieApp/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/avosa/movieApp/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/avosa/movieApp/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/avosa/movieApp/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/avosa/movieApp/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/avosa/movieApp/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/avosa/movieApp/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/avosa/movieApp/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/avosa/movieApp/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/avosa/movieApp/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/avosa/movieApp/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/avosa/movieApp/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/avosa/movieApp/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/avosa/movieApp/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/avosa/movieApp/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/avosa/movieApp/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
